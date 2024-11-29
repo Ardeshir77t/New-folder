34499985d583b04bcc7d5d17e4b26841c21abb8a
ardeshir <ww.ardeshir77t@gmail.com>
2024-11-29 12:42:03 +0330

    adding some css and js and html

--- new.js
+++ new.js
@@ -1,2 +1,5 @@
 console.log(`hello world`);
 console.log(`well what do you expect this is what you would learn `);
+console.log(
+  `ok lets say i made a change so after that i have to commit that into the backup branch and then comtinue my work so let do it`
+);
